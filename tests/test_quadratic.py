fe853660673653e19977ddd0c579c509d773d36d
Sasha-Vlasova <avlasova@carrollu.edu>
2026-06-01 02:50:50 +0000

    Add ccomplex root test

--- tests/test_quadratic.py
+++ tests/test_quadratic.py
@@ -6,8 +6,15 @@ def test_two_real_roots():
     assert roots[0] == 2
     assert roots[1] == 1
     
-def test_two_real_roots():
+def test_repeated_root():
     roots = solve_quadratic(1,-3,2)
 
     assert roots[0] == 1
     assert roots[1] == 1
+
+
+def test_complex_roots():
+    roots = solve_quadratic(1, 2, 5)
+
+    assert roots[0] == (-1 + 2j)
+    assert roots[1] == (-1 - 2j)
